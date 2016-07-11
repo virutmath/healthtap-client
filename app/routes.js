bd7954e9e9fcd3afea6646a7700fe40b2542cfac
Kiên Đặng <kiendt@i-com.vn>
2016-07-11 17:52:27 +0700

    init project

--- app/routes.js
+++ app/routes.js
@@ -5,42 +5,43 @@
 // import { getAsyncInjectors } from 'utils/asyncInjectors';
 
 const errorLoading = (err) => {
-  console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
+	console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
 };
 
 const loadModule = (cb) => (componentModule) => {
-  cb(null, componentModule.default);
+	cb(null, componentModule.default);
 };
 
 export default function createRoutes() {
-  // Create reusable async injectors using getAsyncInjectors factory
-  // const { injectReducer, injectSagas } = getAsyncInjectors(store);
+	// Create reusable async injectors using getAsyncInjectors factory
+	// const { injectReducer, injectSagas } = getAsyncInjectors(store);
 
-  return [
-    {
-      path: '/',
-      name: 'home',
-      getComponent(nextState, cb) {
-        const importModules = Promise.all([
-          System.import('containers/HomePage'),
-        ]);
+	return [
+		{
+			path: '/',
+			name: 'home',
+			getComponent(nextState, cb) {
+				const importModules = Promise.all([
+					System.import('containers/HomePage'),
+				]);
 
-        const renderRoute = loadModule(cb);
+				const renderRoute = loadModule(cb);
 
-        importModules.then(([component]) => {
-          renderRoute(component);
-        });
+				importModules.then(([component]) => {
+          console.log(component);
+					renderRoute(component);
+				});
 
-        importModules.catch(errorLoading);
-      },
-    }, {
-      path: '*',
-      name: 'notfound',
-      getComponent(nextState, cb) {
-        System.import('containers/NotFoundPage')
-          .then(loadModule(cb))
-          .catch(errorLoading);
-      },
-    },
-  ];
+				importModules.catch(errorLoading);
+			},
+		}, {
+			path: '*',
+			name: 'notfound',
+			getComponent(nextState, cb) {
+				System.import('containers/NotFoundPage')
+					.then(loadModule(cb))
+					.catch(errorLoading);
+			},
+		},
+	];
 }
